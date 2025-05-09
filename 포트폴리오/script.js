4c1f3462ec91b42c251d6f189c9f6618309d1e9c
ekwl17 <ekwl17@naver.com>
2025-05-09 16:15:05 +0900

    first commit

--- 포트폴리오/script.js
+++ 포트폴리오/script.js
@@ -1,3 +1,4 @@
+// fullPage.js 초기화
 $(function () {
   $("#fullpage").fullpage({
     autoScrolling: true,
@@ -5,52 +6,22 @@ $(function () {
     easing: "easeInOutCubic",
     easingcss3: "ease",
     scrollingSpeed: 800,
-    // responsiveWidth: 920,
-    // responsiveHeight:900,
     recordHistory: false,
-    anchors: ["page1", "page2", "page3", "page4", "page5", "page6"],
+    anchors: ["page1", "page2", "page3", "page4", "page5"],
     menu: "#pageMenu",
-    // autoScrolling: false,
-
-    onLeave: function (index, nextIndex, direction) {
-      console.log(`onleave!`);
-    },
-    afterLoad: function (anchorLink, index) {
-      console.log(`afterLoad!`);
-    },
-    afterRender: function () {
-      console.log(`afterRender!`);
-    },
-    afterResize: function () {
-      console.log(`afterResize!`);
-    },
-    afterResponsive: function (isResponsive) {
-      console.log(`afterResponsive!`);
-    },
-    afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
-      console.log(`afterSlideLoad!`);
-    },
-    onSlideLeave: function (
-      anchorLink,
-      index,
-      slideIndex,
-      direction,
-      nextSlideIndex
-    ) {
-      console.log(`onSlideLeave!`);
+    afterLoad: function () {
+      AOS.refresh();
     },
   });
 });
 
+// 텍스트 순환
 const spans = document.querySelectorAll("#wordList li span");
 const liItems = document.querySelectorAll("#wordList li");
-
 const classSet = ["on1", "on2", "on3", "on4"];
-const widthSet = [430, 650, 570, 430];
-
+const widthSet = [450, 650, 800, 430];
 let current = 0;
 
-// span에 클래스 붙이기
 function updateSpans(index) {
   spans.forEach((span) => {
     classSet.forEach((cls) => span.classList.remove(cls));
@@ -58,7 +29,6 @@ function updateSpans(index) {
   });
 }
 
-// li의 width 조정
 function updateWidth(index) {
   liItems.forEach((li) => {
     const newWidth = widthSet[index];
@@ -67,15 +37,11 @@ function updateWidth(index) {
   });
 }
 
-// 초기 1회 실행
 updateSpans(current);
 updateWidth(current);
 
-// 3초마다 순환
 setInterval(() => {
-  // transition 잠깐 꺼줌
   liItems.forEach((li) => li.classList.add("no-transition"));
-
   current++;
   if (current === liItems.length - 1) {
     updateSpans(current);
@@ -84,22 +50,19 @@ setInterval(() => {
       current = 0;
       updateSpans(0);
       updateWidth(0);
-    }, 10); 
+    }, 10);
   } else {
     updateSpans(current);
     updateWidth(current);
   }
 
- 
   requestAnimationFrame(() => {
     liItems.forEach((li) => li.classList.remove("no-transition"));
   });
 }, 3000);
 
+// GSAP SplitText
 gsap.registerPlugin(SplitText);
-
-console.clear();
-
 document.fonts.ready.then(() => {
   gsap.set(".split", { opacity: 1 });
 
