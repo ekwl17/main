a0683bfbc0196ca6392f6ae1e0a9a708bb657c4e
ekwl17 <ekwl17@naver.com>
2025-05-13 18:02:51 +0900

    first commit

--- 포트폴리오/script.js
+++ 포트폴리오/script.js
@@ -60,24 +60,3 @@ setInterval(() => {
     liItems.forEach((li) => li.classList.remove("no-transition"));
   });
 }, 3000);
-
-// GSAP SplitText
-gsap.registerPlugin(SplitText);
-document.fonts.ready.then(() => {
-  gsap.set(".split", { opacity: 1 });
-
-  const split = SplitText.create(".split", {
-    type: "words",
-    wordsClass: "word++",
-    wordDelimiter: String.fromCharCode(8205),
-  });
-
-  gsap.from(split.words, {
-    y: -100,
-    opacity: 0,
-    rotation: "random(-60, 60)",
-    stagger: 0.1,
-    duration: 1,
-    ease: "back",
-  });
-});
